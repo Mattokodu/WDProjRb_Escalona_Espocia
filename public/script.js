b30c6fe8dc5a0474f166497b2f769339061567a4
Mattokodu <b2029misespocia@pshs.edu.ph>
2025-12-01 07:47:58 +0800

    something weird

--- public/script.js
+++ public/script.js
@@ -3,12 +3,9 @@
 // ============================================
 function generateNavBar() {
     const navHTML = `
-        <a class="brand" href="index.html">My Site</a>
-        <ul class="nav-list">
-            <li><a href="index.html">Home</a></li>
-            <li><a href="about.html">About</a></li>
-            <li><a href="projects.html">Projects</a></li>
-            <li><a href="contact.html">Contact</a></li>
+        <a class="brand" href="../index.html">My Site</a>
+        <ul>
+            <li><a href="../index.html">Home</a></li>
         </ul>
     `;
     document.getElementById("nav-placeholder").innerHTML = navHTML;
@@ -20,12 +17,13 @@ function generateNavBar() {
 function generateFooter() {
     const footerHTML = `
             <ul>
-                <li><a href="public\\topic1.html">Underlying Themes of Biology</a></li>
-                <li><a href="public\\topic2.html">Mathematical Modelling of Evolution</a></li>
-                <li><a href="public\\topic3.html">Topic 3</a></li>
-                <li><a href="public\\topic4.html">Topic 4</a></li>
+              <li><a href="../index.html">Home Page</a></li>
+              <li><a href="topic2.html">Mathematical Modelling of Evolution</a></li>
+              <li><a href="topic3.html">Topic 3</a></li>
+              <li><a href="topic4.html">Topic 4</a></li>
             </ul>
     `; 
+    
     document.getElementById("footer-placeholder").innerHTML = footerHTML;
 }
 
